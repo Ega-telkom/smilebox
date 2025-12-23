4acb85ed923f1d9bf369c95a599b96eefc4b5c27
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2025-12-23 22:35:21 +0700

    update
    Co-authored-by: VierPoOwiee <ayamk8592@gmail.com>

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("scroll", () => {
+  document.querySelectorAll(".navbar").forEach(navbar => {
+    if (window.scrollY > 50) {
+      navbar.classList.add(
+        "bg-white/30",
+        "backdrop-blur-sm",
+        "border-white/20",
+        "shadow-md"
+      );
+      navbar.classList.remove(
+        "bg-transparent",
+        "border-transparent",
+        "shadow-none"
+      );
+    } else {
+      navbar.classList.remove(
+        "bg-white/30",
+        "backdrop-blur-sm",
+        "border-white/20",
+        "shadow-md"
+      );
+      navbar.classList.add(
+        "bg-transparent",
+        "border-transparent",
+        "shadow-none"
+      );
+    }
+  });
+});
+
